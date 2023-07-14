d54c4780c8bef1eb5f4593f94727cfff5dbe89c6
DongHyun <kimdh0315@naver.com>
2023-07-14 21:56:40 +0900

    서버에서 사용할 함수의 prototype 정의

---
+++ server/server.c
@@ -0,0 +1,89 @@
+#include "server.h"
+
+void init_program(void){
+
+}
+
+void connect_DB(void){
+
+}
+
+void init_client_pool(void){
+
+}
+
+void init_room_pool(void){
+
+}
+
+void room_maker_epoll(void){
+
+}
+
+void add_client_to_pool(){
+
+}
+
+void handle_client(){
+
+}
+
+void client_login(){
+
+}
+
+void client_register(){
+
+}
+
+void create_room(){
+
+}
+
+void add_room_to_pool(){
+
+}
+
+void fetch_information(){
+
+}
+
+void enter_room(){
+
+}
+
+void exit_clien(){
+
+}
+
+void room_main(){
+    
+}
+
+void init_room(){
+
+}
+
+void room_enter_epoll(){
+
+}
+
+void add_player(){
+
+}
+
+void change_player_role(){
+
+}
+
+void change_room_rule(){
+
+}
+
+void start_game(){
+
+}
+
+void exit_room(){
+
+}
---
+++ server/server.h
@@ -0,0 +1,199 @@
+/*
+server.c - prototypes adn definitions for Chess Aplication
+*/
+
+#ifndef __SERVER_H__
+#define __SERVER_H__
+
+/* Include header files */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+#include <setjmp.h>
+#include <signal.h>
+#include <dirent.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <errno.h>
+#include <math.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+/* Define macro functions */
+#define TMP 1024
+
+
+/* Simplifies calls to bind(), connect(), and accept() */
+typedef struct sockaddr SA;
+
+/* Prototypes of Functions */
+
+
+/**
+ * implement : 서버 초기화
+ * input :
+ * output :
+*/
+void init_program(void);
+
+/**
+ * implement : DB와 연결
+ * input :
+ * output :
+*/
+void connect_DB(void);
+
+/**
+ * implement : client pool 초기화, 서버에 접속한 모든 클라이언트 정보 저장
+ * input :
+ * output :
+*/
+void init_client_pool(void);
+
+/**
+ * implement : room pool 초기화, 서버에 만들어진 모든 방 정보 저장
+ * input :
+ * output :
+*/
+void init_room_pool(void);
+
+/**
+ * implement : 방을 만드는 클라이언트 확인
+ * input :
+ * output :
+*/
+void room_maker_epoll(void);
+
+/**
+ * implement : 서버에 접속한 클라이언트를 client_pool에 추가
+ * input :
+ * output :
+*/
+void add_client_to_pool();
+
+/**
+ * implement : 클라이언트의 요청에 따라 맞는 함수들을 호출
+ * input :
+ * output :
+*/
+void handle_client();
+
+/**
+ * implement : 클라이언트의 로그인
+ * input :
+ * output :
+*/
+void client_login();
+
+/**
+ * implement : 클라이언트의 회원가입
+ * input :
+ * output :
+*/
+void client_register();
+
+/**
+ * implement : 클라이언트의 방 만들기 및 방 설정
+ * input :
+ * output :
+*/
+void create_room();
+
+/**
+ * implement : 서버에 만들어진 방을 room_pool에 추가
+ * input :
+ * output :
+*/
+void add_room_to_pool();
+
+/**
+ * implement : 요청된 방 정보를 room_pool에서 가지고 옴
+ * input :
+ * output :
+*/
+void fetch_information();
+
+/**
+ * implement : 방에 들어감
+ * input :
+ * output :
+*/
+void enter_room();
+
+/**
+ * implement : 클라이언트가 서버 연결을 끊는다
+ * input :
+ * output :
+*/
+void exit_clien();
+
+/**
+ * implement : 방을 관리하는 메인함수
+ * input :
+ * output :
+*/
+void room_main();
+
+/**
+ * implement : 방 정보 초기화
+ * input :
+ * output :
+*/
+void init_room();
+
+/**
+ * implement : 방에 들어오는 클라이언트와의 통신을 위한 함수
+ * input :
+ * output :
+*/
+void room_enter_epoll();
+
+/**
+ * implement : accept 된 player를 player_pool에 추가, 역할 부여(p1, p2)
+ * input :
+ * output :
+*/
+void add_player();
+
+/**
+ * implement : 플레이어의 역할을 변경
+ * input :
+ * output :
+*/
+void change_player_role();
+
+/**
+ * implement : 방 설정을 변경
+ * input :
+ * output :
+*/
+void change_room_rule();
+
+/**
+ * implement : 게임을 시작하는 함수
+ * input :
+ * output :
+*/
+void start_game();
+
+/**
+ * implement : 게임이 종료된 후, 해당 방을 제거하기 위함
+ * input :
+ * output :
+*/
+void exit_room();
+
+
+
+#endif __SERVER_H__
