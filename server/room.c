0daa0fa82192fbd7227a5e10e1c0ffc4b6b95b47
DongHyun <kimdh0315@naver.com>
2023-07-18 15:13:13 +0900

    add room.h and room.c

---
+++ server/room.c
@@ -0,0 +1,29 @@
+#include "room.h"
+
+void* room_main(void* args){
+
+}
+
+int init_room(GAME_INFORMATION* gi){
+
+}
+
+int add_player(GAME_INFORMATION* gi){
+
+}
+
+void change_player_role(){
+
+} //보류
+
+void change_room_rule(){
+
+} //보류
+
+void start_game(){
+
+}
+
+void exit_room(pool_room* pr){
+
+}
