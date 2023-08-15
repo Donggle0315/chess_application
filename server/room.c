ace2f4306a2f2db16148269ff548e679c4d1c7e7
DongHyun <kimdh0315@naver.com>
2023-08-16 01:53:36 +0900

    rewrite room functions

--- server/room.c
+++ server/room.c
@@ -11,58 +11,83 @@ int room_main(pool_room* pr, char **arguments, int clientfd, send_info *si){
     }
     // SEL: check if right player(turn), check if right piece(black/white), send moveable
     else if(!strcmp(arguments[0], "SEL")){
-        
+        handle_SEL(room,si,arguments);
     }
     // MOV: check player turn, check right piece, check moveable
     // if moveable -> SUC, send 
     else if(!strcmp(arguments[0], "MOV")){
-        
+        handle_MOV(room,si,arguments);
     }
-    
-
-    int nready;
-    int maxfd = listenfd;
-    int p1fd, p2fd;
-    int player_count = 0;
-    int count = 0;
-    while(1){
-        printf("\ninside room: %d\n", count++);
-
-        ready_set = read_set;
-
-        nready = select(maxfd+1, &ready_set, NULL, NULL, NULL);
-
-        // handle events
-        if(FD_ISSET(listenfd, &ready_set)){
-            int connfd = accept(listenfd, (SA*)clientaddr, &clientlen);
-            bool temp = add_player(gi, connfd, read_set);
-            if(temp == true){
-                player_count++;
-            }
-            
-            fprintf(stdout, "added player in fd: %d\n", connfd);
-            continue;
-        }
-
-        if(player_count == 2){
-            break;
-        }
+    //RES: reset the game
+    else if(!strcmp(arguments[0],"RES")){
+        resetGame(room->b);
+        room->gi->turn=1;
     }
-
-    start_game(gi,read_set,maxfd,p1fd,p2fd);//체스 게임 시작
-
-    exit_room(gi,pr);
-    pthread_exit(0);
+    else if(!strcmp(arguments[0],"EXT")){
+        finishGame(room->b);
+        exit_room(room->gi, pr);
+    }
 }
 
-int start_game(room_option *room, send_info *si){
+void start_game(room_option *room, send_info *si){
     room->gi = init_room();
     room->b = initBoard();
     
     si->send_fds[(si->size)++] = room->player_fd[0];
     si->send_fds[(si->size)++] = room->player_fd[1];
 
-    sendInfoToClient(room->gi, room->b);
+    sendInfoToClient(room, si);
+}
+
+void handle_SEL(room_option *room, send_info *si, char** arguments){
+    //check if right player_turn
+    int turn = atoi(arguments[1]);
+    if(room->gi->turn != turn) {
+        sendMoveableToClient(room,si,NULL,0);
+        return 0;
+    }
+    //get moveable poses
+    int row=atoi(arguments[2][0]);
+    int col=atoi(arguments[2][1]);
+    coordi movealbe_pos[ROW*COL];
+    int moveable_idx=0;
+    getMoveablePosition(room->b,row,col,movealbe_pos,&moveable_idx);
+    
+    //send message to client
+    if(turn%2) si->send_fds[(si->size)++]=room->player_fd[0];
+    else si->send_fds[(si->size)++]=room->player_fd[1];
+    sendMoveableToClient(room,si,movealbe_pos,moveable_idx);
+}
+
+void handle_MOV(room_option *room, send_info *si, char** arguments){
+    //check if right player_turn
+    int turn = atoi(arguments[1]);
+    if(room->gi->turn != turn) {
+        sendIsMoveToClient(room,si,false,false);
+        return 0;
+    }
+    //if the piece can move, then move and change turn
+    int sr=atoi(arguments[2][0]);
+    int sc=atoi(arguments[2][1]);
+    int fr=atoi(arguments[2][2]);
+    int fc=atoi(arguments[2][3]);
+    int deathCode;
+    bool flag=true;
+    if(canMove(room->b,sr,sc,fr,fc)){
+        deathCode=movePiece(room->b,sr,sc,fr,fc,true);
+        if(deathCode) addDeathPiece(room->b,deathCode);
+        changeTurn(room->b);
+        increaseTurnCnt(room->gi);
+    }
+    else{
+        flag=false;
+    }
+    //check if the game finish
+    bool finish = isFinish(room->b);
+    //send message to client
+    if(turn%2) si->send_fds[(si->size)++]=room->player_fd[0];
+    else si->send_fds[(si->size)++]=room->player_fd[1];
+    sendIsMoveToClient(room,si,flag,finish);
 }
 
 GAME_INFORMATION* init_room(){
@@ -84,104 +109,6 @@ void change_room_rule(){
 
 } //보류
 
-void start_game(GAME_INFORMATION* gi, fd_set read_set, int maxfd, int p1fd, int p2fd){
-    chess_board* b=initBoard();
-
-    fd_set ready_set;
-    int nready;
-    int len;
-    char buf[MAX_LEN];
-    int flag=2;
-    while(1){
-        if(isFinish(b)) {//게임이 종료되면 결과를 전송하고 무한 루프 탈출
-            break;
-        }
-        sendInfoToClient(gi,b,p1fd,p2fd);//게임 정보를 클라이언트에게 보냄 "TUR"
-        //read from client 
-        int sr,sc,fr,fc;
-        while(1){//올바른 SEL 문장을 읽어옴
-            int now_player=getNowPlayer(gi,p1fd,p2fd);
-            //현재 턴인 플레이어로부터 SEL 읽어오기
-            while(flag!=0){//flag가 0이면 이미 좌표를 받은 상태임
-                ready_set=read_set;
-                nready=select(maxfd+1,&ready_set,NULL,NULL,NULL);
-                if(nready<0){
-                    fprintf(stderr,"Error in select\n");
-                    return -1;
-                }
-                if(FD_ISSET(now_player,&ready_set)){//현재 순서에 맞는 플레이어가 입력함
-                    len=readall(now_player,buf,MAX_LEN);
-                    //SEL\n#\n이 올바른지 확인 -> 올바르면 break, 틀리면 continue;
-                    if(checkCMD(buf,gi->turn,SEL)){
-                        break;
-                    }
-                }
-            }
-            
-            getCoordinate(len,buf,&sr,&sc,&fr,&fc);//버퍼에서 좌표를 추출
-            if(sr==-1&&sc==-1) {//게임 리셋
-                resetGame(b);
-                break;
-            }
-
-            //자신의 말이 선택되었다면, 해당 말이 갈 수 있는 좌표를 구해 클라이언트에게 전달
-            coordi moveable_pos[ROW*COL];
-            int moveable_idx=0;
-            getMoveablePosition(b,sr,sc,moveable_pos,&moveable_idx);
-            sendMoveableToClient(gi,moveable_pos,moveable_idx,p1fd,p2fd);
-
-            //클라이언트로부터 좌표를 불러와 움직일 수 있다면, 위치로 이동시켜줌
-            while(1){
-                ready_set=read_set;
-                nready=select(maxfd+1,&ready_set,NULL,NULL,NULL);
-                if(nready<0){
-                    fprintf(stderr,"Error in select\n");
-                    return -1;
-                }
-                if(FD_ISSET(now_player,&ready_set)){//현재 순서에 맞는 플레이어가 입력함
-                    len=readall(now_player,buf,MAX_LEN);
-                    //MOV\n#\n이 올바른지 확인
-                    //SEL 이면 다시 위로 올라가야하고, 잘못된 좌표라면 continue, 올바르면 break
-                    if(checkCMD(buf,gi->turn,SEL)){
-                        flag=0;
-                        // getCoordinate(len,buf,&sr,&sc,&fr,&fc);
-                        // if(!canMove(b,sr,sc,fr,fc)){
-                        //     continue;
-                        // }
-                        break;
-                    }
-                    if(checkCMD(buf,gi->turn,MOV)){
-                        flag=1;
-                        getCoordinate(len,buf,&sr,&sc,&fr,&fc);
-                        if(!canMove(b,sr,sc,fr,fc)){//해당 좌표로 이동할 수 없으면, false를 보내고 다시 입력 받도록함
-                            sendIsMoveToClient(gi,false,p1fd,p2fd);
-                            continue;
-                        }
-                        break;//올바른 좌표 정보가 들어왔다면 무한루프 탈출
-                    }
-                }                
-            }
-
-            if(flag==0){//SEL로 돌아가야 함
-                continue;
-            }
-
-            //해당 위치로 말을 움직임
-            int deathCode=movePiece(b,sr,sc,fr,fc,true);
-            sendIsMoveToClient(gi,true,p1fd,p2fd);//해당 위치로 움직였음을 클라이언트에게 보냄
-            if(deathCode){//죽을 말이 있을 경우 추가
-                addDeathPiece(b,deathCode);
-            }
-            break;
-        }
-        //플레이어 턴 변경
-        changeTurn(b);
-        increaseTurnCnt(gi);
-    }
-    //게임 종료
-    sendFinishToClient(gi,b,p1fd,p2fd);
-    finishGame(b);
-}
 
 void increaseTurnCnt(GAME_INFORMATION* gi){
     (gi->turn)++;
@@ -191,135 +118,42 @@ void exit_room(GAME_INFORMATION* gi,pool_room* pr){
     free(gi);
 }
 
-int getNowPlayer(GAME_INFORMATION* gi, int p1fd, int p2fd){
-    if((gi->turn)%2==0) return p2fd;
-    return p1fd;
-}
-
-int getString(char* string,char** buf){
-    char* ptr=strtok(string,"\n");
-    int idx=0;
-    while(ptr!=NULL){
-        strcpy(buf[idx++],ptr);
-        ptr=strtok(NULL,"\n");
-    }
-
-    return idx;
-}
-
-void makeString(char** buf, char* string){
-    int idx=0;
-    while(buf[idx][0]!=0){
-        strcat(string,buf[idx]);
-        strcat(string,"\n");
-        idx++;
-    }
-}
-
-void sendInfoToClient(GAME_INFORMATION* gi, chess_board* b){
-    char buf[MAX_LEN];
-    fprintf(buf,"TUR\n%d\n",gi->turn);
+void sendInfoToClient(room_option *room, send_info *si){
+    fprintf(si->send_string,"TUR\n%d\n",room->gi->turn);
     //보드 위 정보를 저장
     for(int i=0;i<ROW;i++){
         for(int j=0;j<COL;j++){
             char piece[3];
-            fprintf(piece,"%d",b->board[i][j]);
-            strcat(buf,piece);
+            fprintf(piece,"%d",room->b->board[i][j]);
+            strcat(si->send_string,piece);
         }
     }
-    strcat(buf,"\n");
+    strcat(si->send_string,"\n");
 }
 
-void sendMoveableToClient(GAME_INFORMATION* gi, coordi* moveable_pos,int idx,int p1fd,int p2fd){
-    char buf[MAX_LEN];
-    fprintf(buf,"SEL\n%d\n",gi->turn);
+void sendMoveableToClient(room_option *room,send_info* si, coordi* moveable_pos,int idx){
+    fprintf(si->send_string,"SEL\n%d\n",room->gi->turn);
     //좌표 정보 저장
     for(int i=0;i<idx;i++){
         char pos[6];
         fprintf(pos,"%d%d",moveable_pos[i].row,moveable_pos[i].col);
-        strcat(buf,pos);
+        strcat(si->send_string,pos);
     }
-    strcat(buf,"\n");
-
-    writeall(p1fd,buf,MAX_LEN);
-    writeall(p2fd,buf,MAX_LEN);
+    strcat(si->send_string,"\n");
 }
 
-void sendIsMoveToClient(GAME_INFORMATION*gi, bool move,int p1fd,int p2fd){
-    char buf[MAX_LEN];
-    fprintf(buf,"MOV\n%d\n",gi->turn);
+void sendIsMoveToClient(room_option *room, send_info *si, bool move, bool finish){
+    fprintf(si->send_string,"MOV\n%d\n",room->gi->turn);
     if(move){//이동 성공
-        strcat(buf,"1\n");
+        strcat(si->send_string,"SUC\n");
     }
     else{//이동 실패
-        strcat(buf,"0\n");
+        strcat(si->send_string,"FAL\n");
     }
-
-    writeall(p1fd,buf,MAX_LEN);
-    writeall(p2fd,buf,MAX_LEN);
-}
-
-void sendFinishToClient(GAME_INFORMATION* gi,chess_board* b,int p1fd,int p2fd){
-    char buf[MAX_LEN];
-    fprintf(buf,"FIN\n%d\n",gi->turn);
-    if(b->player_turn==BLACK){//white승리
-        strcat(buf,"1\n");
+    if(finish){//게임 끝
+        strcat(si->send_string,"FIN\n");
     }
-    else{//black승리
-        strcat(buf,"2\n");
+    else{//게임 안 끝나
+        strcat(si->send_string,"RES\n");
     }
-
-    writeall(p1fd,buf,MAX_LEN);
-    writeall(p2fd,buf,MAX_LEN);
-}
-
-void getCoordinate(int len, char* buf, int* sr, int*sc,int* fr,int* fc){
-    int idx=0;
-    int newLineCnt=2;
-    // 좌표가 있는 곳(개행문자 2번 뒤)까지 접근
-    while(newLineCnt!=0){
-        if(buf[idx++]=='\n'){
-            newLineCnt--;
-        }
-    }
-    //좌표 정보를 저장
-    if('0'<=buf[idx] && buf[idx]<='9'){
-        *sr=(buf[idx]-'0');
-        *sc=(buf[idx+1]-'0');
-        if('0'<=buf[idx+2] && buf[idx+2]<='9'){
-            *fr=(buf[idx+2]-'0');
-            *fc=(buf[idx+3]-'0');
-        }
-        else{
-            *fr=-1;
-            *fc=-1;
-        }
-    }
-}
-
-bool checkCMD(char* buf,int turn, int cmd){
-    //cmd가 맞는지 확인
-    bool cmdFlag=false;
-    switch(cmd){
-        case SEL:
-            if(!strncmp(buf,"SEL",3)){
-                cmdFlag=true;
-            }
-            break;
-        case MOV:
-            if(!strncmp(buf,"MOV",3)){
-                cmdFlag=true;
-            }
-            break;
-    }
-
-    //턴수가 맞는지 확인
-    int tur=0;
-    for(int i=4;buf[i]!='\n';i++){
-        tur*=10;
-        tur+=(buf[i]-'0');
-    }
-    //결과 반환
-    if(tur==turn && cmdFlag) return true;
-    return false;
 }
--- server/room.h
+++ server/room.h
@@ -16,125 +16,95 @@ room.c - prototypes adn definitions for room
 #include "chess.h"
 
 
-
-
-
 /**
- * implement : 방을 관리하는 메인함수(스레드에서 실행) : 
- * input : pool_room pointer
+ * implement : 
+ * input : 
  * output :
 */
 int room_main(pool_room*, char**, int, send_info*);
 
 /**
- * implement : 방 정보 초기화 : 체스와 관련된 코드를 실행, GAME_INFORMATION 초기화
- * input : x
- * output : GAME_INFORMATION pointer
+ * implement : 
+ * input : 
+ * output :
+*/
+void start_game(room_option*, send_info*);
+
+/**
+ * implement : 
+ * input : 
+ * output :
+*/
+void handle_SEL(room_option*,send_info*,char**);
+
+/**
+ * implement : 
+ * input : 
+ * output :
+*/
+void handle_MOV(room_option*, send_info*,char**);
+
+/**
+ * implement : 
+ * input : 
+ * output :
 */
 GAME_INFORMATION* init_room();
 
 /**
- * implement : accept 된 player를 GAME_INFORMATION에 추가, 역할 부여(p1, p2, 관전)
- * input : GAME_INFORMATION*
- * output :int 성공(TRUE)/실패(FALSE)
-*/
-int add_player(GAME_INFORMATION*,int,fd_set);
-
-/**
- * implement : 플레이어의 역할을 변경
- * input :
+ * implement : 
+ * input : 
  * output :
-*/
-void change_player_role(); //보류
-
-/**
- * implement : 방 설정을 변경
- * input :
- * output :
-*/
-void change_room_rule(); //보류
-
-/**
- * implement : 게임을 시작하는 함수
- * input :
- * output :
-*/
-void start_game(GAME_INFORMATION*,fd_set,int,int,int);
-
-/**
- * implement : GAME_INFORMATION에 저장된 턴수를 증가시키는 함수
- * input : GAME_INFORMATION pointer
- * output : void
 */
 void increaseTurnCnt(GAME_INFORMATION*);
 
 /**
- * implement : 게임이 종료된 후, 해당 방을 제거하기 위함 : room_pool에서 해당 방 제거
- * input : pool_room pointer
- * output : void
+ * implement : 
+ * input : 
+ * output :
 */
-void exit_room(GAME_INFORMATION*, pool_room*);
+void exit_room(GAME_INFORMATION*,pool_room*);
 
 /**
- * implement : 현재 플레이해야하는 클라이언트의 fd를 알려주는 함수
- * input : GAME_INFORMATION pointer, p1fd, p2fd
- * output : 현재 턴인 플레이어 fd
+ * implement : 
+ * input : 
+ * output :
 */
-int getNowPlayer(GAME_INFORMATION*, int, int);
+void sendInfoToClient(room_option*,send_info*);
 
 /**
- * implement : 인자로 들어온 문자열을 '\n'을 기준으로 나눠 버퍼에 저장하는 함수
- * input : 나눌 문자열(char*), 문자열을 저장할 버퍼(char*[])
- * output : 버퍼에 저장된 문자열 개수(idx)
+ * implement : 
+ * input : 
+ * output :
 */
-int getString(char*,char**);
+void sendMoveableToClient(room_option*, send_info*,coordi*, int);
 
 /**
- * implement : 인자로 들어온 문자열을 '\n'을 기준으로 구분하여 하나의 문자열을 만드는 함수
- * input : 나눌 문자열 배열(char*[]), 문자열을 저장할 버퍼(char*)
- * output : x
+ * implement : 
+ * input : 
+ * output :
 */
-void makeString(char**,char*);
+void sendIsMoveToClient(room_option*, send_info*,bool,bool);
 
 /**
- * implement : TUR의 정보를 보내주는 함수(턴 수, 보드 정보)
- * input : 게임정보, 보드 정보, p1fd, p2fd
- * output : x
+ * implement : 
+ * input : 
+ * output :
 */
-void sendInfoToClient(GAME_INFORMATION*,chess_board*,int,int);
+int add_player(GAME_INFORMATION*,int,fd_set);
 
 /**
- * implement : SEL의 정보를 보내주는 함수(턴 수, 이동 가능한 좌표)
- * input : 게임정보, 좌표 정보, p1fd, p2fd
- * output : x
+ * implement : 
+ * input : 
+ * output :
 */
-void sendMoveableToClient(GAME_INFORMATION*,coordi*,int,int,int);
+void change_player_role();
 
 /**
- * implement : MOV의 정보를 보내주는 함수(턴 수, 이동 여부) -> 이동 성공시 1, 실패시 0을 버퍼에 저장
- * input : 게임정보, 이동 성공 여부(bool), p1fd, p2fd
- * output : x
+ * implement : 
+ * input : 
+ * output :
 */
-void sendIsMoveToClient(GAME_INFORMATION*,bool,int,int);
+void change_room_rule();
 
-/**
- * implement : FIN의 정보를 보내주는 함수(이긴 사람 여부)
- * input : 보드 정보, p1fd, p2fd
- * output : x
-*/
-void sendFinishToClient(GAME_INFORMATION*,chess_board*,int,int);
-
-/**
- * implement : 버퍼에 저장된 좌표 정보를 얻어오는 함수 / sr,sc만 버퍼에 있다면 fr,fc는 -1로 초기화
- * input : len, buf, sr, sc, fr, fc
- * output : x
-*/
-void getCoordinate(int,char*,int*,int*,int*,int*);
-
-/**
- * implement : 버퍼에 저장된 함수가 원하는 명령어, 턴수에 맞는지 확인하는 함수
- * input : buf, turn(int), cmd(int)
- * output : 다 충족하면 true, 아니면 false
-*/
-bool checkCMD(char*,int,int);
 #endif
